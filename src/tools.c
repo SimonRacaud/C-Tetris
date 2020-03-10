6d352df1de48cfd4551a2963561acdd78de5a72f
RACAUD Simon <simonracaud@gmail.com>
2020-03-10 09:21:28 +0100

    Feat(tools function) : merge_path_filename : merge two str (a dir path and a file name)

--- src/tools.c
+++ src/tools.c
@@ -39,3 +39,25 @@ int my_get_check_nbr(char *str, int *nbr, const char *ignore)
     (*nbr) = my_getnbr(str);
     return EXIT_SUCCESS;
 }
+
+char *merge_path_filename(const char *path, const char *filename)
+{
+    int sizea = my_strlen(path);
+    int sizeb = my_strlen(filename);
+    char *merge = malloc(sizeof(char) * (sizea + sizeb + 2));
+
+    if (!merge) {
+        my_putstr_error("ERORR: malloc : merge_str() \n");
+        return NULL;
+    }
+    if (path[0] != '\0')
+        my_strcpy(merge, path);
+    if (path[sizea - 1] != '/' && path[0] != '\0') {
+        merge[sizea] = '/';
+        sizea++;
+    }
+    if (filename[0] != '\0')
+        my_strcpy(&merge[sizea], filename);
+    merge[sizea + sizeb] = '\0';
+    return merge;
+}
