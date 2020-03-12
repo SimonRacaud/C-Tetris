ec135ceee665a912a1c8eacca3405eb7c2790f51
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-03-12 09:23:24 +0100

    - Add special key file

---
+++ src/get_special_key.c
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2019
+** tetris
+** File description:
+** tetris
+*/
+
+#include <term.h>
+#include "tetris.h"
+#include "config_t.h"
+
+extern const int NB_KEY;
+extern const char *MYTERM;
+extern const char **SPECIAL_KEYS;
+
+static char *my_get_term(char **env)
+{
+    for (int i = 0; env[i]; i++)
+        if (my_strncmp(env[i], MYTERM, 5) == 0)
+            return my_strdup(env[i] + 5);
+    return NULL;
+}
+
+static char **get_array(void)
+{
+    char **key = malloc(sizeof(char *) * (NB_KEY + 1));
+
+    if (!key)
+        return NULL;
+    key[NB_KEY] = NULL;
+    for (int i = 0; i < NB_KEY; i++) {
+        key[i] = my_strdup(tigetstr(SPECIAL_KEYS[i]));
+        if (!key[i])
+            return NULL;
+    }
+    return key;
+}
+
+char **get_special_key(char **env)
+{
+    int my_errret = 0;
+    char *my_term = my_get_term(env);
+
+    if (!my_term)
+        return NULL;
+    setupterm(my_term, 1, &my_errret);
+    free(my_term);
+    if (my_errret == -1)
+        return 0;
+    return get_array();
+}
