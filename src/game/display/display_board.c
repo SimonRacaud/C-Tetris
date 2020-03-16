6bbaee7644b20b47380c5ef4f6e1c5dc5163636c
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-03-16 10:17:29 +0100

    - Fix display game with color

--- src/game/display/display_board.c
+++ src/game/display/display_board.c
@@ -7,13 +7,27 @@
 
 #include "tetris.h"
 
+extern char BACKG_CHAR;
+extern char PIECE_CHAR;
+
+static void display_map(game_t *tetris, window_t *board, int x, int y)
+{
+    if (tetris->map[y][x] != BACKG_CHAR) {
+        wattron(board->w, COLOR_PAIR(tetris->map[y][x]));
+        mvwprintw(board->w, 2 + y, 1 + x, "%c", PIECE_CHAR);
+        wattroff(board->w, COLOR_PAIR(tetris->map[y][x]));
+    }
+    else
+        mvwprintw(board->w, 2 + y, 1 + x, "%c", tetris->map[y][x]);
+}
+
 int display_board(game_t *tetris)
 {
     window_t *board = &tetris->board;
 
     for (int y = 0; y < tetris->conf.map_height; y++) {
         for (int x = 0; x < tetris->conf.map_width; x++)
-            mvwprintw(board->w, 2 + y, 1 + x, "%c", tetris->map[y][x]);
+            display_map(tetris, board, x, y);
         mvwprintw(board->w, 1, 1, "%c", '\n');
     }
     display_player_pieces(tetris, board);
