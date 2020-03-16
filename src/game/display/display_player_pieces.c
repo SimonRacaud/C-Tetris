c38008b7a20beeeac514ab8c828a0a3825f4eb86
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-03-16 10:01:44 +0100

    - Add player piece display with 7 colors

--- src/game/display/display_player_pieces.c
+++ src/game/display/display_player_pieces.c
@@ -9,11 +9,27 @@
 
 extern char PIECE_CHAR;
 
+static void color_init(void)
+{
+    start_color();
+    init_pair(1, COLOR_YELLOW, 0);
+    init_pair(2, COLOR_CYAN, 0);
+    init_pair(3, COLOR_WHITE, 0);
+    init_pair(4, COLOR_RED, 0);
+    init_pair(5, COLOR_BLUE, 0);
+    init_pair(6, COLOR_GREEN, 0);
+    init_pair(7, COLOR_MAGENTA, 0);
+}
+
 static void display_tetriminos(game_t *tetris, window_t *board, int x, int y)
 {
-    if (tetris->ppiece.piece->mtx[y][x] == PIECE_CHAR)
-        mvwprintw(board->w, tetris->ppiece.coord.y + y,
-            tetris->ppiece.coord.x + x, "%c", tetris->ppiece.piece->mtx[y][x]);
+    color_init();
+    if (tetris->ppiece.piece->mtx[y][x] == PIECE_CHAR) {
+        wattron(board->w, COLOR_PAIR(tetris->ppiece.piece->color));
+        mvwaddch(board->w, tetris->ppiece.coord.y + y,
+            tetris->ppiece.coord.x + x, tetris->ppiece.piece->mtx[y][x]);
+        wattroff(board->w, COLOR_PAIR(tetris->ppiece.piece->color));
+    }
 }
 
 void display_player_pieces(game_t *tetris, window_t *board)
