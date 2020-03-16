079355601849d1a13eac10807c83e8f142a6420f
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-03-16 16:52:46 +0100

    - Fix uninitialized value

--- src/prepare/game_init.c
+++ src/prepare/game_init.c
@@ -47,6 +47,7 @@ int game_init(game_t *tetris, char **argv, int argc, char **env)
     tetris->ppiece.coord.x = 4; // DEBUG
     tetris->ppiece.coord.y = 4; // DEBUG
     tetris->ppiece.is_fall = true; // DEBUG
+    tetris->ppiece.speed_y = 1; // DEBUG
     game_init_struct(tetris);
     return EXIT_SUCCESS;
 }
