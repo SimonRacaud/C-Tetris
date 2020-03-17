330db6e5447b64b6e6c7e9a04ca8613676cf8464
RACAUD Simon <simonracaud@gmail.com>
2020-03-17 19:29:19 +0100

    FEAT(piece_is_in_map_range) : check if the player piece is outside the map edges

---
+++ src/piece/piece_is_in_map_range.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2020
+** PROJECT_NAME
+** File description:
+** Created: 2020-03-17T11:50:39+01:00 | Author: simon
+*/
+
+#include "tetris.h"
+
+bool piece_is_in_map_range(game_t *tetris, player_piece_t *piece)
+{
+    coord_t map = {tetris->conf.map_width, tetris->conf.map_height};
+
+    if (piece->coord.x < 0 || piece->coord.x >= map.x) {
+        return false;
+    } else if (piece->coord.y < 0 || piece->coord.y >= map.y) {
+        return false;
+    }
+    if ((piece->coord.x + piece->piece->width) < 0 ||
+        (piece->coord.x + piece->piece->width) >= map.x) {
+        return false;
+    } else if ((piece->coord.y + piece->piece->height) < 0 ||
+        (piece->coord.y + piece->piece->height) >= map.y) {
+        return false;
+    }
+    return true;
+}
