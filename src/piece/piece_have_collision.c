ef97a8eb5168df83e3c5324c5ef0b672810e149d
RACAUD Simon <simonracaud@gmail.com>
2020-03-17 19:28:08 +0100

    FEAT(piece_have_collision) : check if the current position of the player piece will cause a collision

---
+++ src/piece/piece_have_collision.c
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2020
+** PROJECT_NAME
+** File description:
+** Created: 2020-03-16T21:04:36+01:00 | Author: simon
+*/
+
+#include "tetris.h"
+
+extern const char PIECE_CHAR;
+
+static bool have_collision_with_other_piece(game_t *tetris,
+player_piece_t *piece)
+{
+    size_t end_point_y = piece->coord.y + piece->piece->height;
+    size_t end_point_x = piece->coord.x + piece->piece->width;
+    size_t piece_x = 0;
+    size_t piece_y = 0;
+
+    for (size_t y = piece->coord.y; y < end_point_y; y++) {
+        for (size_t x = piece->coord.x; x < end_point_x; x++) {
+            piece_x = x - piece->coord.x;
+            piece_y = y - piece->coord.y;
+            if (tetris->map[y][x] == PIECE_CHAR &&
+                    piece->piece->mtx[piece_y][piece_x] == PIECE_CHAR) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool piece_have_collision(game_t *tetris)
+{
+    player_piece_t *pp = &tetris->ppiece;
+
+    if (!piece_is_in_map_range(tetris, pp)) {
+        return true;
+    }
+    if (have_collision_with_other_piece(tetris, pp)) {
+        return true;
+    }
+    return false;
+}
