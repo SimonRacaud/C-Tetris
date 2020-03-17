226f5a0535bd25b03706750a6b0d35018bca2dcb
RACAUD Simon <simonracaud@gmail.com>
2020-03-17 19:24:18 +0100

    Fix(write_on_map) : write piece color on map and not the character of the piece

--- src/piece/write_on_map.c
+++ src/piece/write_on_map.c
@@ -7,39 +7,20 @@
 
 #include "tetris.h"
 
-static void write_piece(game_t *tetris, player_piece_t *piece)
-{
-    size_t end_point_y = piece->coord.y + piece->piece->height;
-    size_t end_point_x = piece->coord.x + piece->piece->width;
-    size_t piece_x = 0;
-    size_t piece_y = 0;
+extern const char PIECE_CHAR;
 
-    for (size_t y = piece->coord.y; y < end_point_y; y++) {
-        for (size_t x = piece->coord.x; x < end_point_x; x++) {
-            piece_x = x - piece->coord.x;
-            piece_y = y - piece->coord.y;
-            tetris->map[y][x] = piece->piece->mtx[piece_y][piece_x];
+static void write_piece(game_t *tetris, player_piece_t *ppiece)
+{
+    size_t end_point_y = ppiece->coord.y + ppiece->piece->height;
+    size_t end_point_x = ppiece->coord.x + ppiece->piece->width;
+
+    for (size_t y = ppiece->coord.y; y < end_point_y; y++) {
+        for (size_t x = ppiece->coord.x; x < end_point_x; x++) {
+            if (tetris->map[y][x] == PIECE_CHAR) {
+                tetris->map[y][x] = ppiece->piece->color;
+            }
         }
     }
-}
-
-static bool piece_is_in_map_range(game_t *tetris, player_piece_t *piece)
-{
-    coord_t map = {tetris->conf.map_width, tetris->conf.map_height};
-
-    if (piece->coord.x < 0 || piece->coord.x >= map.x) {
-        return false;
-    } else if (piece->coord.y < 0 || piece->coord.y >= map.y) {
-        return false;
-    }
-    if ((piece->coord.x + piece->piece->width) < 0 ||
-        (piece->coord.x + piece->piece->width) >= map.x) {
-        return false;
-    } else if ((piece->coord.y + piece->piece->height) < 0 ||
-        (piece->coord.y + piece->piece->height) >= map.y) {
-        return false;
-    }
-    return true;
 }
 
 int write_on_map(game_t *tetris)
