9d616bdb1c66ee40c832cd5569d62a3207b9cfb7
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-03-26 09:57:03 +0100

    - Fix norm

--- src/prepare/clean_piece.c
+++ src/prepare/clean_piece.c
@@ -11,7 +11,7 @@ static void rm_space(tetrimino_t *piece, int y)
 {
     for (int x = piece->width - 1; x >= 0; x--)
         if (piece->mtx[y][x] == '*') {
-                piece->mtx[y][x + 1] = '\0';
+            piece->mtx[y][x + 1] = '\0';
             break;
         }
 }
